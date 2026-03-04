7afa61ff40fbd21a261e2df28f1e4a8b3d3905d4
Padmore Adusei Ening <padmore27@gmail.com>
2026-03-04 07:52:06 +0000

    Build a Simple Company Website with React

---
+++ my-company/src/App.jsx
@@ -0,0 +1,26 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Services from './components/Services';
+import Contact from './components/Contact';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path='' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/services' element={<Services />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+
+
+    </BrowserRouter>
+  );
+}
+
+export default App
