4660184317f51f9ddffa3abca7c204b427e73d37
Padmore Adusei Ening <padmore27@gmail.com>
2026-03-04 07:55:07 +0000

    Build a Simple Company Website with React

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,14 +2,21 @@ import { Link } from "react-router-dom";
 
 const Navbar =()=>{
 
-    return(
-        <nav>
-            <Link to='/' >Home</Link>
-            <Link to='/about'>About</Link>
-            <Link to='/services'>Services</Link>
-            <Link to='/contact'>Contact</Link>
-        </nav>
-    )
+    return (
+      <nav
+        style={{
+          backgroundColor: "black",
+          display: "flex",
+          justifyContent: "space-between",
+          padding: "10px",
+        }}
+      >
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/services">Services</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    );
 }
 
 export default Navbar
