dc2ffd71e1e8b5a7972e7ab29f6dd13c38bff7fe
Padmore Adusei Ening <padmore27@gmail.com>
2026-03-16 18:16:34 +0000

    initialized recipe app with zustand

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,16 +1,25 @@
-import { create } from "zustand";
+import create from "zustand";
 
 export const useRecipeStore = create((set) => ({
   recipes: [],
-
   addRecipe: (newRecipe) =>
-    set((state) => ({
-      recipes: [...state.recipes, newRecipe],
-    })),
-  setRecipe: (recipes) => set({ recipes }),
-
-  deleteRecipe: (id) =>
-    set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
-    })),
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes }),
 }));
+
+// import { create } from "zustand";
+
+// export const useRecipeStore = create((set) => ({
+//   recipes: [],
+
+//   addRecipe: (newRecipe) =>
+//     set((state) => ({
+//       recipes: [...state.recipes, newRecipe],
+//     })),
+//   setRecipe: (recipes) => set({ recipes }),
+
+//   deleteRecipe: (id) =>
+//     set((state) => ({
+//       recipes: state.recipes.filter((recipe) => recipe.id !== id),
+//     })),
+// }));
