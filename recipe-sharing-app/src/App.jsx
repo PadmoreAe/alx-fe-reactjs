a9408d3012c17230c79b65c10b007775046ec38c
Padmore Adusei Ening <padmore27@gmail.com>
2026-03-17 04:55:58 +0000

    Expand the Recipe Sharing App

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,34 +1,36 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import RecipeList from "./components/RecipeList";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
 import AddRecipeForm from "./components/AddRecipeForm";
 
 function App() {
   return (
     <>
-      {/* Always visible */}
-      <Header />
+      <Router>
+        {/* Always visible */}
+        <Header />
 
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <div className="mt-36">
-                <AddRecipeForm />
-              </div>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <div className="mt-36">
+                  <AddRecipeForm />
+                </div>
 
-              <div className="mb-96">
-                <RecipeList />
-              </div>
-            </>
-          }
-        />
-      </Routes>
+                <div className="mb-96">
+                  <RecipeList />
+                </div>
+              </>
+            }
+          />
+        </Routes>
 
-      {/* Always visible */}
-      <Footer />
+        {/* Always visible */}
+        <Footer />
+      </Router>
     </>
   );
 }
